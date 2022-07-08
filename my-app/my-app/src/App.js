0288ab1c8e049fe59d3f6353d6270d861f68d8dc
wairimujosephine001 <wairimujosephine001@gmail.com>
2022-07-07 20:45:46 -0400

    first commit

---
+++ my-app/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Portfolio from './Component/Portfolio';
+
+function App() {
+  return (
+    <div>
+     < Portfolio />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/my-app/src/Component/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap';
+
+export const Portfolio = () => {
+  return (
+      <div>
+        <h1 className='text-center text-slate-400'>Navbar</h1>
+      </div>
+  )
+}
+
+export default Portfolio;
